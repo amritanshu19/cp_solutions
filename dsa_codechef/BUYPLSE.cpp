b10e2de746f06a12b1d487a1ce443e68e98fd66c
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-03 21:19:47 +0530

    Create BUYPLSE.cpp

---
+++ dsa_codechef/BUYPLSE.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/CCSTART2/problems/BUYPLSE
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, x, y;
+    cin>>a>>b>>x>>y;
+    cout<<a*x+b*y<<endl;
+    return 0;
+}
