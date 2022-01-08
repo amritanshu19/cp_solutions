d9d1b9e8974715b168501218e9bf574916430461
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-09 01:06:52 +0530

    Create 490A - Team Olympiad.cpp

---
+++ codeforces/490A - Team Olympiad.cpp
@@ -0,0 +1,38 @@
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+int n,pr=0, m=0, pe=0;
+cin>>n;
+int t[n];
+vector<int>pro, mat, spo;
+for(int i=0; i<n;i++)
+{
+    cin>>t[i];
+    if(t[i]==1)
+    {
+        pr++;
+        pro.push_back(i+1);
+    }
+    else if(t[i]==2)
+    {
+        m++;
+        mat.push_back(i+1);
+    }
+    else
+    {
+        pe++;
+        spo.push_back(i+1);
+    }
+}
+int w = min(pr,min(m,pe));
+cout<<w<<'\n';
+for(int i=0; i<w;i++)
+{
+    cout<<pro[i]<<" "<<mat[i]<<" "<<spo[i]<<'\n';
+}
+
+    return 0;
+}
