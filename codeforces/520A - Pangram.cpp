25f0c9cebb372560ed3e65bba111f5e6ee1a1948
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-13 11:39:58 +0530

    Create 520A - Pangram.cpp

---
+++ codeforces/520A - Pangram.cpp
@@ -0,0 +1,45 @@
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int n; cin>>n;
+    string s;
+    cin>>s;
+    if(n<26)
+    {
+        cout<<"NO\n";
+    }
+    else{
+        int c2=0;
+        for(int i=0; i<n;i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        for(int i=0; i<n;i++)
+        {
+            for(int j=i+1; j<n;j++)
+        {
+            if(s[i]==s[j])
+            {
+                c2++;
+                break;
+            }
+        }
+        }
+        
+        if(n-c2>=26)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+          
+    }
+    return 0;
+}
