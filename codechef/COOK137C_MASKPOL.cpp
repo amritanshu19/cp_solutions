f1bc3cafe7e7c93cb4cd35dbad2585f81a2288cd
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-23 23:00:48 +0530

    Create COOK137C_MASKPOL.cpp

---
+++ codechef/COOK137C_MASKPOL.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/COOK137C/problems/MASKPOL
+#include<bits/stdc++.h>
+
+
+using namespace std;
+ 
+int main()
+{
+  int T; cin>>T;
+  while(T--)
+  {
+    int N,A;
+    cin>>N>>A;
+    cout<<min(A,N-A)<<endl;
+
+  }
+  
+  return 0;
+}
