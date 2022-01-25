7de636de9948be6c3efd7e64a1a662bb2bf6cbff
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-25 23:12:38 +0530

    Create 1370A - Maximum GCD.cpp

---
+++ codeforces/1370A - Maximum GCD.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/1370/A
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+  int T; cin>>T;
+  while(T--)
+  {
+    int n; cin>>n;
+    cout<<n/2<<endl;
+  }
+  return 0;
+}
