17cf755fa67f285341cff7af997bdf807aed76ce
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-27 21:35:20 +0530

    Create START23C_CHFPROFIT.cpp

---
+++ codechef/START23C_CHFPROFIT.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/START23C/problems/CHFPROFIT
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+  
+    int T; cin>>T;
+    while(T--)
+    {
+      int X, Y,Z;
+      cin>>X>>Y>>Z;
+      cout<<X*1ll*(Z-Y)<<'\n';
+    }
+    return 0;
+    
+}
