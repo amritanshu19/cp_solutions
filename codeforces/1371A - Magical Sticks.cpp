0bd0c59230a16dd6ea8f75a23e05ddee060426d6
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-27 21:59:57 +0530

    Create 1371A - Magical Sticks.cpp

---
+++ codeforces/1371A - Magical Sticks.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/problemset/problem/1371/A
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+  
+    int t; cin>>t;
+    while(t--)
+    {
+      int n; cin>>n;
+      if(n%2==0)
+      {
+        cout<<n/2<<'\n';
+      }
+      else
+      {
+        cout<<n/2+1<<'\n';
+      }
+    }
+    return 0;
+    
+}
