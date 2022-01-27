48abb0569bbdac6247ab8614ad1409d835541dfb
Amritanshu Raj <83136054+amritanshu19@users.noreply.github.com>
2022-01-27 21:36:57 +0530

    Create START23C_AVGOF3.cpp

---
+++ codechef/START23C_AVGOF3.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/START23C/problems/AVGOF3
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+  
+    int T; cin>>T;
+    while(T--)
+    {
+      int X; cin>>X;
+      cout<<X<<' '<<X-1<<' '<<X+1<<'\n';
+    }
+    return 0;
+    
+}
